102a8b00b36945146ea8107baa7b07b018dd1573
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-12-19 14:39:53 -0500

    Completes contract with funding and withdrawl.

--- FundMe.sol
+++ FundMe.sol
@@ -8,6 +8,7 @@ contract FundMe {
     
     AggregatorV3Interface internal priceFeed;
     address public owner;
+
     constructor() {
         owner = msg.sender;
         // priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
@@ -15,10 +16,12 @@ contract FundMe {
     }
 
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
 
     function fund() public payable {
         uint256 minimumUSD = 5 * 10 ** 18;
         require(minimumUSD <= getConversionRate(msg.value), "You need more eth");
+        funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
         // What is the eth to usd conversion rate.
         // How do we get the data of the conversion rate.
@@ -46,5 +49,10 @@ contract FundMe {
     function withdraw() payable onlyOwner public {
         require(msg.sender == owner);
         payable(msg.sender).transfer(address(this).balance);
+        for (uint256 fidx = 0; i < funders.length;  fidx++){
+            address funderAdrs = funders[fidx];
+            addressToAmountFunded[funderAdrs] = 0;
+        }
+        funders = new address[];
     }
 }
